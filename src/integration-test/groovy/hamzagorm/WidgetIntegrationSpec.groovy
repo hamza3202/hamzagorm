f428673b96510456ff2eccf59bfad02d17b8d7e7
Jeff Scott Brown <brownj@objectcomputing.com>
2018-09-04 14:08:28 -0600

    comment

--- src/integration-test/groovy/hamzagorm/WidgetIntegrationSpec.groovy
+++ src/integration-test/groovy/hamzagorm/WidgetIntegrationSpec.groovy
@@ -13,6 +13,7 @@ class WidgetIntegrationSpec extends Specification {
     @Rollback
     void 'test unique constraint'() {
         expect:
+        // just verifying that GORM is accessible as expected in this @Rollback method
         new Widget(name: 'Widget One').save()
         new Widget(name: 'Widget Two').save()
         !new Widget(name: 'Widget One').save()
