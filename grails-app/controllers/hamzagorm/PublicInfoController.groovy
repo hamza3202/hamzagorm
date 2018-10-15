5463770269fe7c2880ef0dddb5142ab2d58d041d
Muhammad Hamza Zaib <hamza@aurorasolutions.io>
2018-10-15 17:19:31 +0500

    Hyphenated URLs test

---
+++ grails-app/controllers/hamzagorm/PublicInfoController.groovy
@@ -0,0 +1,12 @@
+package hamzagorm
+
+class PublicInfoController {
+
+    def dataPrivacy() {
+
+    }
+
+    def dataPrivacyViewSpecified() {
+        render view: 'dataPrivacyViewSpecified'
+    }
+}
